31162ddeb89f058e970b52e83c1378b206baa9a7
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2025-02-26 21:46:20 +0700

    Done Analyse Services

--- diary-analysis-service/src/models/Diary.ts
+++ diary-analysis-service/src/models/Diary.ts
@@ -2,5 +2,73 @@ import mongoose from "mongoose";
 
 const diarySchema = new mongoose.Schema(
     {
-    }
-)
+        senderId: {
+            type: String,
+        },
+        content: {
+            type: String,
+        },
+        emotionObjects: [
+            {
+                type: Object,
+                emotionLevel: {
+                    type: String,
+                },
+                emotionCategory: {
+                    type: String,
+                },
+                emotionSummary: {
+                    type: String,
+                }
+            }
+        ],
+        correlationObjects: [
+            {
+                type: Object,
+                name: {
+                    type: String,
+                },
+                description: {
+                    type: String,
+                },
+            }
+        ],
+
+        symptomObjects: [
+            {
+                type: Object,
+                name: {
+                    type: String,
+                },
+                risk: {
+                    type: String,
+                },
+                description: {
+                    type: String,
+                },
+                suggestions: {
+                    type: String,
+                }
+            }
+        ],
+        recommendations: [
+            {
+                type: Object,
+                practice: {
+                    type: String,
+                },
+                action: {
+                    type: String,
+                }
+            }
+        ],
+        imageLink: [
+            String
+        ]
+    },
+    {timestamps: true}
+);
+
+const Diary = mongoose.models.diaries || mongoose.model("diaries", diarySchema);
+
+export default Diary;
